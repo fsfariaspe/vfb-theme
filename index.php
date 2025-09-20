24e30c5ef7c779dd214fd748e53996d81a6ed146
Flávio Farias <76065757+fsfariaspe@users.noreply.github.com>
2025-09-20 20:05:48 -0300

    retirei cod do motor de busca

--- index.php
+++ index.php
@@ -7,11 +7,7 @@ get_header();
   <h1>Bem-vindo à nova página inicial!</h1>
   <p>Este é um texto de exemplo para testar o layout limpo do tema.</p>
 
-  <!-- Motor de Busca Befly -->
-  <div id="wrapper" style="margin: 40px auto; max-width: 600px;">
-    <befly-widget language="pt-br" new-tab="true"></befly-widget>
-  </div>
-  <script type="text/javascript" src="https://static.onetravel.com/widget/search/production/widget-befly.js"></script>
+  
 </main>
 
 <?php get_footer(); ?>
