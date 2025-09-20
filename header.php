8e001b7162bad8198c9676ba5b63d58e12cdcc8d
Flávio Farias <76065757+fsfariaspe@users.noreply.github.com>
2025-09-20 18:38:16 -0300

    fix: garantir enqueue do CSS customizado

--- header.php
+++ header.php
@@ -22,6 +22,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     
     <?php wp_head(); ?>
+        <?php
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/styles.css">
 </head>
 
 <body <?php body_class('font-sans'); ?>>
