e4bbdaa80c0a0185078a32b1c21cd53fa7739ec5
Flávio Farias <76065757+fsfariaspe@users.noreply.github.com>
2025-09-20 20:45:44 -0300

    style: ajustar altura do iframe na home

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ get_header();
 <main id="main-content" style="padding: 60px 20px; text-align: center;">
   <h1>Pacotes Forfait (Queensberry)</h1>
   <iframe src="https://api.queensberry.com.br/Caderno/forfait"
-          style="width:100%;min-height:900px;border:none;"
+          style="width:100vw;height:calc(100vh - 180px);min-height:900px;border:none;display:block;margin:0 -10vw;"
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"></iframe>
 </main>
